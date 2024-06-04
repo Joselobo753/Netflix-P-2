a4c091b498dfb0fe6f3b8a15e504e38cbec5ec36
Santiago Puertas <santidoors13@gmail.com>
2024-06-04 02:44:32 -0300

    Arreglado ABM y funcion eliminarPelicula

--- js/admin/app.js
+++ js/admin/app.js
@@ -1,106 +1,107 @@
-import { validateImg, validateTexto, validateUrl } from "../validadores.js";
+import {
+  validateImg,
+  validateTexto,
+  validateUrl,
+  validateNombre,
+} from "../validadores.js";
 import { agregarPelicula, crearCategoria, editarPelicula } from "./abm.js";
-import { cargarTablaPeliculas, cargartabla, estaEditando, leerCategorias } from "./ultis.js";
-
-
+import {
+  cargarTablaPeliculas,
+  cargartabla,
+  estaEditando,
+  leerCategorias,
+} from "./ultis.js";
 
 cargarTablaPeliculas();
 
+const $form = document.getElementById("formulario");
+const $inputNombre = document.getElementById("nombre");
+const $inputTipo = document.getElementById("tipo");
+const $inputCategoria = document.getElementById("categoria");
+const $inputCaratula = document.getElementById("caratula");
+const $inputTrailer = document.getElementById("trailer");
+const $inputDescripcion = document.getElementById("descripcion");
 
+$inputNombre.addEventListener("blur", () => {
+  validateNombre($inputNombre);
+});
+$inputCaratula.addEventListener("blur", () => {
+  validateImg($inputCaratula);
+});
+$inputTrailer.addEventListener("blur", () => {
+  validateUrl($inputTrailer);
+});
+$inputDescripcion.addEventListener("blur", () => {
+  validateTexto($inputDescripcion);
+});
 
-const $form = document.getElementById("formulario")
-const $inputNombre = document.getElementById("nombre")
-const $inputTipo = document.getElementById("tipo")
-const $inputCategoria = document.getElementById("categoria")
-const $inputCaratula = document.getElementById("caratula")
-const $inputTrailer = document.getElementById("trailer")
-const $inputDescripcion = document.getElementById("descripcion")
+$form.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    !validateNombre($inputNombre) ||
+    !validateImg($inputCaratula) ||
+    !validateUrl($inputTrailer) ||
+    !validateTexto($inputDescripcion)
+  ) {
+    alert("Revisar los campos");
+    return;
+  }
+  const titulo = $inputNombre.value;
+  const tipo = $inputTipo.value;
+  const categoria = $inputCategoria.value;
+  const caratula = $inputCaratula.value;
+  const trailer = $inputTrailer.value;
+  const descripcion = $inputDescripcion.value;
+  if (estaEditando()) {
+    console.log(descripcion);
+    agregarPelicula(titulo, tipo, categoria, caratula, trailer, descripcion);
+  } else {
+    editarPelicula(titulo, tipo, categoria, caratula, trailer, descripcion);
+  }
 
-$inputNombre.addEventListener("blur",()=>{
-    validateTexto($inputNombre)
-})
-$inputCaratula.addEventListener("blur",()=>{
-    validateImg($inputCaratula)
-})
-$inputTrailer.addEventListener("blur",()=>{
-    validateUrl($inputTrailer)
-})
-$inputDescripcion.addEventListener("blur",()=>{
-    validateTexto($inputDescripcion)
-})
-
-$form.addEventListener("click",(e)=>{
-    e.preventDefault();
-     /*if (
-        !validateTexto($inputNombre)||
-        !validateImg($inputCaratula)||
-        !validateUrl($inputTrailer)||
-        !validateTexto($inputDescripcion)
-    ) {
-        alert("Revisar los campos")
-        return;
-    } */
-   const titulo = $inputNombre.value
-    const tipo = $inputTipo.value
-    const categoria = $inputCategoria.value
-    const caratula = $inputCaratula.value
-    const trailer = $inputTrailer.value
-    const descripcion = $inputDescripcion.value
- if (estaEditando()) {
-    console.log(descripcion)
-    agregarPelicula(titulo, tipo, categoria, caratula, trailer, descripcion) 
-    
- }else{
-    editarPelicula(titulo,tipo,categoria,caratula,trailer,descripcion)
-}
-   
-
-
-    document.getElementById("formularioPeliculas").reset();
-  $inputNombre.classList.remove('is-valid', 'is-invalid');
-  $inputTipo.classList.remove('is-valid', 'is-invalid');
-  $inputCaratula.classList.remove('is-valid', 'is-invalid');
-  $inputTrailer.classList.remove('is-valid', 'is-invalid');
-  $inputDescripcion.classList.remove('is-valid', 'is-invalid');
-
-  
+  document.getElementById("formularioPeliculas").reset();
+  $inputNombre.classList.remove("is-valid", "is-invalid");
+  $inputTipo.classList.remove("is-valid", "is-invalid");
+  $inputCaratula.classList.remove("is-valid", "is-invalid");
+  $inputTrailer.classList.remove("is-valid", "is-invalid");
+  $inputDescripcion.classList.remove("is-valid", "is-invalid");
 
   cargarTablaPeliculas();
-
-  
 
   let mensaje = `Nueva pelicula creada`;
 
   swal.fire({
-    title: 'Exito',
+    title: "Exito",
     text: mensaje,
-    icon: 'success',
+    icon: "success",
     showConfirmButton: true,
     showCancelButton: false,
-    confirmButtonText: 'Genial',
+    confirmButtonText: "Genial",
   });
 });
 
-document.getElementById('categoriaForm').addEventListener('submit', function(event) {
+document
+  .getElementById("categoriaForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    const nombre = document.getElementById('nombreCat').value;
-    const calificacion = document.getElementById('calificacion').value;
-    crearCategoria(nombre,calificacion);
+    const nombre = document.getElementById("nombreCat").value;
+    const calificacion = document.getElementById("calificacion").value;
+    crearCategoria(nombre, calificacion);
     mostrarCategorias();
-    cargartabla()
-    document.getElementById('categoriaForm').reset();
-});
+    cargartabla();
+    document.getElementById("categoriaForm").reset();
+  });
 
 export function mostrarCategorias() {
-    const categorias = leerCategorias();
-    const selectCategoria = document.getElementById('categoria');
-    selectCategoria.innerHTML = '';
-    categorias.forEach(categoria => {
-        const option = document.createElement('option');
-        option.value = categoria.nombre;
-        option.textContent = categoria.nombre;
-        selectCategoria.appendChild(option);
-    });
+  const categorias = leerCategorias();
+  const selectCategoria = document.getElementById("categoria");
+  selectCategoria.innerHTML = "";
+  categorias.forEach((categoria) => {
+    const option = document.createElement("option");
+    option.value = categoria.nombre;
+    option.textContent = categoria.nombre;
+    selectCategoria.appendChild(option);
+  });
 }
 mostrarCategorias();
-cargartabla()
+cargartabla();
--- js/validadores.js
+++ js/validadores.js
@@ -2,57 +2,66 @@ import { trimInterno, validInput, invalidInput } from "./utils.js";
 
 export const validateTexto = ($field) => {
   if (!$field || !$field.value.trim()) {
-    $field.classList.add("is-invalid");
-    $field.classList.remove("is-valid");
+    invalidInput($field);
+    return false;
+  }
+
+  if ($field.value.trim().length < 3 || $field.value.trim().length > 50) {
+    invalidInput($field);
     return false;
   }
 
   const regex = /^[a-zA-ZÁÉÍÓÚáéíóúÜüÑñ\s]+$/;
   if (!regex.test($field.value)) {
-    $field.classList.add("is-invalid");
-    $field.classList.remove("is-valid");
+    invalidInput($field);
     return false;
   }
 
-  $field.classList.remove("is-invalid");
-  $field.classList.add("is-valid");
+  validInput($field);
   return true;
 };
 
 export const validateUrl = ($field) => {
   if (!$field || !$field.value.trim()) {
-    $field.classList.add("is-invalid");
-    $field.classList.remove("is-valid");
+    invalidInput($field);
     return false;
   }
 
   if ($field.value.trim().length < 3) {
-    $field.classList.add("is-invalid");
-    $field.classList.remove("is-valid");
+    invalidInput($field);
     return false;
   }
+
+  const regex = /^(ftp|http|https):\/\/[^ "]+$/;
+  if (!regex.test($field.value)) {
+    invalidInput($field);
+    return false;
+  }
+
+  validInput($field);
+  return true;
 };
 
 export const validateImg = ($field) => {
-  const regex =
-    /https?:\/\/(?:www\.)?[-\w@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-\w@:%_\+.~#?&//=]*)\.(?:jpg|jpeg|png|gif|bmp)/;
   if (!$field || !$field.value.trim()) {
-    $field.classList.add("is-invalid");
-    $field.classList.remove("is-valid");
+    invalidInput($field);
     return false;
   }
 
+  const regex =
+    /https?:\/\/(?:www\.)?[-\w@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-\w@:%_\+.~#?&//=]*)\.(?:jpg|jpeg|png|gif|bmp)/;
   if (!regex.test($field.value)) {
-    $field.classList.add("is-invalid");
-    $field.classList.remove("is-valid");
+    invalidInput($field);
     return false;
   }
 
   if ($field.value.trim().length < 3) {
-    $field.classList.add("is-invalid");
-    $field.classList.remove("is-valid");
+    invalidInput($field);
     return false;
   }
+
+  validInput($field);
+  return true;
 };
 
 export const validateNombre = ($input) => {
